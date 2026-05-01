041d2c537ff2097f2941e824a53eeab21df4070b
Ahmed Elshenawy <120160756+a-Elshenawy@users.noreply.github.com>
2026-05-02 00:13:34 +0300

    Update translate.py

--- utils/translate.py
+++ utils/translate.py
@@ -4,7 +4,7 @@ def is_arabic(text):
     return any('\u0600' <= c <= '\u06FF' for c in text)
 
 def to_english(text):
-    return GoogleTranslator(source="auto", target="en").translate(text)
+    return GoogleTranslator(source="ar", target="en").translate(text)
 
 def to_arabic(text):
-    return GoogleTranslator(source="auto", target="ar").translate(text)
+    return GoogleTranslator(source="en", target="ar").translate(text)
